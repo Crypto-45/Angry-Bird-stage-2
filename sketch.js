b686fb7a4ec52caca23205df4a49bd532ae43830
Crypto-45 <bijaysikdar29@gmail.com>
2020-10-31 22:12:41 -0700

    classActivity

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +17,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    stick = new Log(250,130,70,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -35,6 +37,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    chain = new Chain(bird.body,stick.body);
 
 }
 
@@ -61,4 +64,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    stick.display();
+    chain.display();
 }
